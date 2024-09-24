0b0f54fd65b838c96182450e710c943f929f80a4
Bruno Leão <132297721+brunorafaelleao@users.noreply.github.com>
2024-09-24 13:47:07 -0400

    Update ProcessosDeDesign.mk

--- ProcessosDeDesign.mk
+++ ProcessosDeDesign.mk
@@ -1,1 +1,6 @@
 1) Um designer tende a resolver problemas
+-> Designer busca resolver um ou mais problemas
+
+2) Processos de design
+  a) Entender o usuário e o problema
+  b) Entender necessidades e simplificar o escopo
