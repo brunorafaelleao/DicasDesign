eb150e2352762b45caa123e23d97cf99835a6b29
Bruno Leão <132297721+brunorafaelleao@users.noreply.github.com>
2024-09-24 13:49:18 -0400

    Update ProcessosDeDesign.mk

--- ProcessosDeDesign.mk
+++ ProcessosDeDesign.mk
@@ -3,4 +3,7 @@
 
 2) Processos de design
   a) Entender o usuário e o problema
-  b) Entender necessidades e simplificar o escopo
+  b) Entender necessidades e simplificar o escopo (preciso realmente colocar a idade?)
+  c) Desenhar experiência do produto
+  d) Prototipar, testar e validar experiência projetada
+  e) Lançar, mensurar e melhorar
