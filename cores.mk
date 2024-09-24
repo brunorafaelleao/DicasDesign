75feef262ffa623e4048124d92adf6535c7d6355
Bruno Leão <132297721+brunorafaelleao@users.noreply.github.com>
2024-09-24 14:05:29 -0400

    Update cores.mk

--- cores.mk
+++ cores.mk
@@ -1,3 +1,5 @@
 1) Definir uma cor específica
+
 2) Regra para definir a harmonia das cores
  a) 60 / 30 / 10 -> Utilizar em 60% do espaço uma cor unificadora, 30% cor secundária e em 10% uma cor de destaque
+ b) Depois de definir a cor, usar uma ferramenta para verificar quem mais fez um trabalho com a cor (ex: dribbble.com)
