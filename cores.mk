96238238e7fd8d8c73f7ea574618bd4ef1f9c83e
Bruno Leão <132297721+brunorafaelleao@users.noreply.github.com>
2024-09-24 13:52:36 -0400

    Create cores.mk

---
+++ cores.mk
@@ -0,0 +1,3 @@
+1) Definir uma cor específica
+2) Regra para definir a harmonia das cores
+ a) 60 / 30 / 10 -> Utilizar em 60% do espaço uma cor unificadora, 30% cor secundária e em 10% uma cor de destaque
