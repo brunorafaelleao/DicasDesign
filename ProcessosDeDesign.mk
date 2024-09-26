77599947d94e909e8309e664c30e6cfbe9f7e3d8
Bruno Leão <132297721+brunorafaelleao@users.noreply.github.com>
2024-09-26 16:33:34 -0400

    Update ProcessosDeDesign.mk

--- ProcessosDeDesign.mk
+++ ProcessosDeDesign.mk
@@ -7,3 +7,33 @@
   c) Desenhar experiência do produto
   d) Prototipar, testar e validar experiência projetada
   e) Lançar, mensurar e melhorar
+
+-> Os processos de design não são lineares, mas é possível delinear uma estrutura geral que é iterativa
+
+-> Estrutura genérica dos processos de design o entendimento e a experimentação
+  a) Entendimento: Pode ser por meio de diversas técnicas como observação, entrevistas, dinâmicas de consenso, questionários, pesquisas de mercado etc.
+  b) Experimentação: Envolve duas etapas, a de prototipação e a de validação dos protótipos.
+
+-> Estrutura geral do projeto:
+  a) Entender o problema - qual o contexto? quem são as pessoas? como é resolvido atualmente
+      Ao mapear pode-se descobrir que o problema é diferente do que fora apresentado
+
+3) Processos de Design Centrado no Usuário
+-> Mantém o foco nas necessidades, desejos e dificuldades das pessoas. É preciso conhecer o usuário
+
+-> Divide-se em 4 etapas:
+   a) Entender o contexto de uso: Após identificar quem são as pessoas que vão usar o produto, é fundamental compreender em qual contexto ele será utilizado. É um produto para ser usado        no trabalho ou para lazer? Quais as emoções gerais das pessoas nesse contexto? Estão incomodadas com algo e precisam registrar uma reclamação estando em uma loja? Estão perdidas e        com medo na rua, procurando um endereço? Estão em casa apressadas e vão realizar um pagamento?
+
+   b) Definir requisitos: Após compreender o contexto, é necessário fazer um levantamento das necessidades e dos pontos de conflito das pessoas usuárias por meio de pesquisas, como             questionários, entrevistas e observações.
+
+   c) Projetar soluções: A partir do conhecimento do contexto e requisitos, é chegado o momento de projetar soluções. Ou seja, de criar hipóteses para solucionar o que foi previamente          identificado. Essas alternativas são desenvolvidas por meio de protótipos.
+
+   d) Avaliar resultados: Os protótipos são colocados em testes com as pessoas usuárias. A partir desses testes é possível coletar o que funcionou e o que é necessário melhorar.
+
+
+4) Processo Double Diamond
+-> Segue quatro premissas:
+    a) Ser centrado nas pessoas;
+    b) Comunicar-se (visualmente e inclusivamente);
+    c) Colaborar e cocriar; e
+    d) Repetir, repetir e repetir
